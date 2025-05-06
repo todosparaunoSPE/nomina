7a079c5154d0a343c75c861af23ba74e79a93360
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-05-06 17:09:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,23 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 # Título
 title = "Dashboard de Compensaciones y Nómina"
 st.title(title)
